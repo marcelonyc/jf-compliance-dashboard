f2d6935744c2cf943db223335c2f267464b70078
marcelonyc <marcelo.litovsky@gmail.com>
2024-12-01 11:36:30 -0500

    Start with scheduler

--- src/scripts/start-dash.sh
+++ src/scripts/start-dash.sh
@@ -8,9 +8,9 @@ cd /app/src
 
 # Start job queue workers
 # 3 parallel workers
-rq worker -u redis://redis:6379/1 &
-rq worker -u redis://redis:6379/1 &
-rq worker -u redis://redis:6379/1 &
+rq worker --with-scheduler -u redis://redis:6379/1 &
+rq worker --with-scheduler -u redis://redis:6379/1 &
+rq worker --with-scheduler -u redis://redis:6379/1 &
 
 rqscheduler --host redis --port 6379 --db 1 &
 
